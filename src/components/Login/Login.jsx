a1555831f023698eb0387c43e6676ba40fff95c7
devsamin <abdullahalsamin79@gmail.com>
2025-08-03 02:31:57 -0700

    recape createing user email password and error messages

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,50 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import auth from "../../__auth_firebase";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 
 const Login = () => {
+  const [errMessage, seterrMessage] = useState("");
 
-    const hendelsubmit = (e) =>{
-        e.preventDefault()
-        const email = e.target.email.value;
-        const password = e.target.password.value;
-        console.log(email, password)
+  const hendelsubmit = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(email, password);
+    seterrMessage("");
 
-        createUserWithEmailAndPassword(auth, email, password)
-        .then(res => {
-          console.log(res.user)
-        })
-        .catch(err => console.log(err))
-    }
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((res) => {
+        console.log(res.user);
+      })
+      .catch((err) => {
+        console.log(err);
+        seterrMessage(err.message);
+      });
+  };
   return (
     <div>
       <h2>Login</h2>
 
       <form onSubmit={hendelsubmit}>
-            <div className="hero bg-base-200 min-h-screen">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          
-          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-            <div className="card-body">
-              <fieldset className="fieldset">
-                <label className="label">Email</label>
-                <input type="email" name="email" className="input" placeholder="Email" />
-                <label  className="label">Password</label>
-                <input
-                  type="password"
-                  className="input"
-                  placeholder="Password"
-                  name="password"
-                />
-                <div>
-                  <a className="link link-hover">Forgot password?</a>
-                </div>
-                <button className="btn btn-neutral mt-4">Login</button>
-              </fieldset>
+        <div className="hero bg-base-200 min-h-screen">
+          <div className="hero-content flex-col lg:flex-row-reverse">
+            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+              <div className="card-body">
+                <fieldset className="fieldset">
+                  <label className="label">Email</label>
+                  <input
+                    type="email"
+                    name="email"
+                    className="input"
+                    placeholder="Email"
+                  />
+                  <label className="label">Password</label>
+                  <input
+                    type="password"
+                    className="input"
+                    placeholder="Password"
+                    name="password"
+                  />
+                  <div>
+                    <a className="link link-hover">Forgot password?</a>
+                  </div>
+                  <button className="btn btn-neutral mt-4">Login</button>
+                  {errMessage && <p className="text-red-600">{errMessage}</p>}
+                </fieldset>
+              </div>
             </div>
           </div>
         </div>
-      </div>
       </form>
     </div>
   );
